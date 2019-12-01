a828a19feb718ce686e9b33d2bd3da5fd701f9a1
Andrey Pechkurov <apechkurov@gmail.com>
2019-12-01 21:47:31 +0300

    Introduce pool size limit

--- benchmark/benchmark.js
+++ benchmark/benchmark.js
@@ -2,7 +2,8 @@
 
 const Pool = require('../src/pool');
 
-const poolSize = 2 * 1024 * 1024;
+const sourceSize = 2 * 1024 * 1024;
+const poolSize = 100;
 const allocSizes = [8 * 1024, 16 * 1024, 32 * 1024, 64 * 1024, 128 * 1024, 256 * 1024, 512 * 1024];
 const allocsPerIteration = 1024;
 const iterations = 1024;
@@ -14,14 +15,14 @@ const args = process.argv.slice(2);
 const type = args[0] || 'pool-2mb';
 switch (type) {
   case 'no-pool-2mb':
-    Buffer.poolSize = poolSize;
+    Buffer.poolSize = sourceSize;
     allocFn = (size) => Buffer.allocUnsafe(size);
     break;
   case 'no-pool-def':
     allocFn = (size) => Buffer.allocUnsafe(size);
     break;
   default:
-    pool = new Pool(poolSize);
+    pool = new Pool(sourceSize, poolSize);
     allocFn = (size) => pool.allocUnsafe(size);
 }
 
@@ -81,7 +82,7 @@ async function runAll() {
     console.log(`Benchmark run finished: size=${allocSize}, time=${result.time}, rate=${result.rate}`);
     if (pool) {
       console.log('Pool stats: ', pool.stats());
-      pool = new Pool(poolSize);
+      pool = new Pool(sourceSize);
     }
   }
 }
--- src/pool.js
+++ src/pool.js
@@ -4,7 +4,8 @@ const rootSliceSymbol = Symbol('rootSlice');
 
 class Pool {
 
-  _size;
+  _sourceSize;
+  _poolSize;
   _fg;
   _sourcePoolRef = new WeakRef([]);
 
@@ -15,15 +16,16 @@ class Pool {
   _reusedCnt = 0;
   _allocatedCnt = 0;
 
-  constructor(size) {
-    this._size = size;
+  constructor(sourceSize, poolSize) {
+    this._sourceSize = sourceSize;
+    this._poolSize = poolSize;
     this._fg = new FinalizationGroup(this._reclaim.bind(this));
     this._createSource();
   }
 
   allocUnsafe = (size) => {
-    if (size < (this._size >>> 1)) {
-      if (size > (this._size - this._offset)) {
+    if (size < (this._sourceSize >>> 1)) {
+      if (size > (this._sourceSize - this._offset)) {
         this._createSource();
       }
       const slice = this._rootSlice.slice(this._offset, this._offset + size);
@@ -48,7 +50,7 @@ class Pool {
     if (source) {
       this._reusedCnt++;
     } else {
-      source = Buffer.allocUnsafeSlow(this._size);
+      source = Buffer.allocUnsafeSlow(this._sourceSize);
       this._allocatedCnt++;
     }
     this._rootSlice = source.slice();
@@ -68,6 +70,9 @@ class Pool {
     const sourcePool = this._sourcePoolRef.deref() || [];
     this._sourcePoolRef = new WeakRef(sourcePool);
     for (const source of iter) {
+      if (sourcePool.length >= this._poolSize) {
+        return;
+      }
       this._reclaimedCnt++;
       sourcePool.push(source);
     }
